25137bb092ecbc5029e8014e7b189bc422935b3c
wallacesintra <wallvxwallace962@gmail.com>
2026-02-25 01:08:15 +0300

    Update JDK to 11 in jitpack.yml, enable JitPack repository, and update Gradle configurations.

--- writeIt/build.gradle.kts
+++ writeIt/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
     id("maven-publish")
 }
 
@@ -27,6 +28,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+//    kotlinOptions {
+//        jvmTarget = "11"
+//    }
     buildFeatures {
         compose = true
     }
